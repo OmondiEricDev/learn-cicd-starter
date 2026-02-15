ef822c0f53c5f3b24bc2085181f3457ee0910d94
Eric Omondi <>
2026-02-15 11:36:50 +0900

    add test for GetAPIKey function

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -3,9 +3,7 @@ package auth
 import (
 	"errors"
 	"net/http"
-	"reflect"
 	"strings"
-	"testing"
 )
 
 var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
@@ -22,12 +20,4 @@ func GetAPIKey(headers http.Header) (string, error) {
 	}
 
 	return splitAuth[1], nil
-}
-
-func TestGetAPIKey(t *testing.T) {
-	got, _ := GetAPIKey(http.Header{"Authorization": []string{"ApiKey 12345"}})
-	want := "12345"
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("GetAPIKey() = %v, want %v", got, want)
-	}
 }
